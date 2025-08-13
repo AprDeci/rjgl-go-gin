fb7b2aaed814aee483e57346a21ea7e4b4ff9354
Lushichen <lushichen@linakesi.com>
2025-08-13 15:35:38 +0800

    refactor(affairs): 移除 AffairsService 结构体并直接使用 models.DB
    将 CreateApprovalTemplate 方法从 AffairsService 结构体移出，改为直接使用 models.DB 进行操作

--- service/affairs.go
+++ service/affairs.go
@@ -1,16 +1,12 @@
 package service
 
 import (
+	"github.com/aprdec/rjgl/models"
 	"github.com/aprdec/rjgl/models/proj"
-	"gorm.io/gorm"
 )
 
-type AffairsService struct {
-	DB *gorm.DB
-}
-
-func (s *AffairsService) CreateApprovalTemplate(template *proj.ApprovalTemplate) (bool, error) {
-	if err := s.DB.Create(template).Error; err != nil {
+func CreateApprovalTemplate(template *proj.ApprovalTemplate) (bool, error) {
+	if err := models.DB.Create(template).Error; err != nil {
 		return false, err
 	}
 	return true, nil
